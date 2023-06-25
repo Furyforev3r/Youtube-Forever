5207aa132467c775c11e92e9e2939639aa62ee3e
FuryForever <messiasfuryforever@gmail.com>
2023-06-25 11:38:22 -0300

    refac

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import { Request, Response } from "express";
 
 const app = express()
 const port = process.env.PORT || 3000
+const tempDir = "tmp"
 
-app.get('/download/:link', async (req, res) => {
+app.get('/download/:link', async (req: Request, res: Response) => {
   const linkParam = req.params.link
   const link = `https://www.youtube.com/watch?v=${linkParam}`
 
@@ -15,15 +16,15 @@ app.get('/download/:link', async (req, res) => {
   const filename = `${info.videoDetails.title} - ${info.videoDetails.author}.mp3`
 
   ytdl(link, { format: audioFormat })
-    .pipe(fs.createWriteStream(filename))
+    .pipe(fs.createWriteStream(`${tempDir}/${filename}`))
     .on('finish', () => {
-      res.download(filename, () => {
-        fs.unlinkSync(filename)
+      res.download(`${tempDir}/${filename}`, () => {
+        fs.unlinkSync(`${tempDir}/${filename}`)
       })
     })
 })
 
-app.get('/videoinfo/:link', async (req, res) => {
+app.get('/videoinfo/:link', async (req: Request, res: Response) => {
   const linkParam = req.params.link
   const link = `https://www.youtube.com/watch?v=${linkParam}`
 
