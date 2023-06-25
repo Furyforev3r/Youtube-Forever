71963289720384aac8427bbab897f33c38008a9c
FuryForever <messiasfuryforever@gmail.com>
2023-06-25 11:58:31 -0300

    ytdl change

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,53 @@
 import express from 'express'
-import ytdl from 'ytdl-core'
-import fs from 'fs'
-import { Request, Response } from "express";
+import ytdl from 'ytdl-core-discord'
+import { Request, Response } from 'express'
 
 const app = express()
 const port = process.env.PORT || 3000
-const tempDir = "tmp"
 
 app.get('/download/:link', async (req: Request, res: Response) => {
-  const linkParam = req.params.link
-  const link = `https://www.youtube.com/watch?v=${linkParam}`
+  try {
+    const linkParam = req.params.link
+    const link = `https://www.youtube.com/watch?v=${linkParam}`
 
-  const info = await ytdl.getInfo(link)
-  const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' })
-  const filename = `${info.videoDetails.title} - ${info.videoDetails.author}.mp3`
+    const info = await ytdl.getInfo(link)
+    const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' })
+    const filename = `${info.videoDetails.title} - ${info.videoDetails.author}.mp3`
 
-  ytdl(link, { format: audioFormat })
-    .pipe(fs.createWriteStream(`${tempDir}/${filename}`))
-    .on('finish', () => {
-      res.download(`${tempDir}/${filename}`, () => {
-        fs.unlinkSync(`${tempDir}/${filename}`)
-      })
-    })
+    res.set({
+      'Content-Disposition': `attachment; filename="${filename}"`,
+      'Content-Type': 'audio/mpeg'
+    });
+
+    (await ytdl(link, { format: audioFormat })).pipe(res)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to download the audio' })
+  }
 })
 
 app.get('/videoinfo/:link', async (req: Request, res: Response) => {
-  const linkParam = req.params.link
-  const link = `https://www.youtube.com/watch?v=${linkParam}`
+  try {
+    const linkParam = req.params.link
+    const link = `https://www.youtube.com/watch?v=${linkParam}`
 
-  const info = await ytdl.getInfo(link)
+    const info = await ytdl.getInfo(link)
 
-  res.json({
-    author: info.videoDetails.author,
-    title: info.videoDetails.title,
-    views: info.videoDetails.viewCount,
-    thumbnail: info.videoDetails.thumbnails
-  })
+    res.json({
+      author: info.videoDetails.author,
+      title: info.videoDetails.title,
+      views: info.videoDetails.viewCount,
+      thumbnail: info.videoDetails.thumbnails
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to fetch video information' })
+  }
 })
 
-app.get('/', async (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.json({
-    query: "None"
+    query: 'None'
   })
 })
 
